84b3413a4855eb1a1fc961c98b615bb1ac79d962
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2021-09-18 18:04:36 +0700

    try visualize the boards

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
     public static void createConwayGame(int size) {
         // It's basically a grid, there may be an initial configuration like the grid size
+        Boolean [][] conwayBoards = new Boolean[size][size];
 
-        Boolean [][] conwayMaps = new Boolean[size][size];
+        // How to start the data? Do I need to start in the middle of the boards?
+        for (int i = 0; i < conwayBoards.length; i++) {
+            System.out.println(Arrays.toString(conwayBoards[i]));
+        }
     }
 
     public static void main(String[] args) {
