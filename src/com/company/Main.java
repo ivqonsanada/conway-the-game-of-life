b34954ce0aa3cd7fdf8107d46a4be41a265f69b2
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2021-09-18 23:02:22 +0700

    still figuring out how to update the view after the entire generation updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,13 @@ public class Main {
     public static void createConwayGame(int size) {
         // It's basically a grid, there may be an initial configuration like the grid size
         String [][] conwayBoards = new String[size][size];
-        String [][] tempBoards = new String[size][size];
 
         // How to start the data? Do I need to start in the middle of the boards?
 
         // Set up the initial values for the boards
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                conwayBoards[i][j] = "dead";
+                conwayBoards[i][j] = "#";
             }
         }
 
@@ -29,16 +28,22 @@ public class Main {
         }
 
         System.out.println();
-
-
-
+        
         // is the boards updated as a whole or its being checked on every cell and get updated?
         for (int k = 0; k < 3; k++) {
+            String [][] tempBoards = new String[size][size];
+
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    tempBoards[i][j] = conwayBoards[i][j];
+                }
+            }
+
             for (int i = (size / 2) - 2; i < (size / 2) + 2; i++) {
                 for (int j = 0; j < size; j++) {
                     int liveCells = countLiveNeighbourCells(i, j, conwayBoards);
-                    if (liveCells < 2 || liveCells > 3) tempBoards[i][j] = "dead";
-                    else tempBoards[i][j] = "live";
+                    if (liveCells < 2 || liveCells > 3) tempBoards[i][j] = "#";
+                    else tempBoards[i][j] = "O";
                 }
             }
             for (int i = 0; i < size; i++) {
@@ -51,14 +56,14 @@ public class Main {
     public static int countLiveNeighbourCells(int i, int j, String[][] boards) {
         int counter = 0;
         try {
-            if (boards[i-1][j-1] == "live") counter++;
-            if (boards[i-1][j] == "live") counter++;
-            if (boards[i-1][j+1] == "live") counter++;
-            if (boards[i][j-1] == "live") counter++;
-            if (boards[i][j+1] == "live") counter++;
-            if (boards[i+1][j-1] == "live") counter++;
-            if (boards[i+1][j] == "live") counter++;
-            if (boards[i+1][j+1] == "live") counter++;
+            if (boards[i-1][j-1] == "O") counter++;
+            if (boards[i-1][j] == "O") counter++;
+            if (boards[i-1][j+1] == "O") counter++;
+            if (boards[i][j-1] == "O") counter++;
+            if (boards[i][j+1] == "O") counter++;
+            if (boards[i+1][j-1] == "O") counter++;
+            if (boards[i+1][j] == "O") counter++;
+            if (boards[i+1][j+1] == "O") counter++;
         } catch (Exception e) {}
         return counter;
     }
