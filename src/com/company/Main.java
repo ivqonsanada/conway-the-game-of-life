8cdee4b16086c5522a062c5de7353c0c8eaa81f6
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2021-09-18 17:54:42 +0700

    figuring out how to create the canvas (grid to show the conway game board

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 
 public class Main {
-    public void createConway() {
+    public static void createConwayGame(int size) {
+        // It's basically a grid, there may be an initial configuration like the grid size
 
+        Boolean [][] conwayMaps = new Boolean[size][size];
     }
 
     public static void main(String[] args) {
-
+        createConwayGame(4);
     }
 }
