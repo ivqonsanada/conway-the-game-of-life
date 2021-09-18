d1d16488508ef3a55426cfea54cfc831be33b5b9
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2021-09-18 18:48:04 +0700

    changed the data type to string for better visualize the boards

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,20 @@ import java.util.Arrays;
 public class Main {
     public static void createConwayGame(int size) {
         // It's basically a grid, there may be an initial configuration like the grid size
-        Boolean [][] conwayBoards = new Boolean[size][size];
+        String [][] conwayBoards = new String[size][size];
 
         // How to start the data? Do I need to start in the middle of the boards?
 
         // Set up the initial values for the boards
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                conwayBoards[i][j] = false;
+                conwayBoards[i][j] = "dead";
             }
         }
 
         for (int i = (size / 2) - 1; i < (size / 2) + 1 ; i++) {
             for (int j = (size / 2) - 1; j < (size / 2) + 1; j++) {
-                conwayBoards[i][j] = true;
+                conwayBoards[i][j] = "live";
             }
         }
 
