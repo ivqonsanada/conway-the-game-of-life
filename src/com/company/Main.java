9c6f779177b31f34f32f72ca349f8ee4b148c6d0
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2021-09-18 17:44:58 +0700

    try make a method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
 public class Main {
+    public void createConway() {
+
+    }
 
     public static void main(String[] args) {
-	// write your code here
-        
+
     }
 }
