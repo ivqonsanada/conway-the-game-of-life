a5d518a305f1b976cd049d4d9e372ec18fb940e9
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2021-09-20 02:02:38 +0700

    still trying to figure out

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -36,31 +36,29 @@ public class Board extends JFrame {
 
         add(panel, BorderLayout.CENTER);
 
-        Timer timer = new Timer(3000, new ActionListener() {
+        Timer timer = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent evt) {
                 if (panel.isShowing()) {
                     // just realize this method only check the middle board, it was because I only try to avoid array out of bound exception
                     for (int i = 0; i < size; i++) {
                         for (int j = 0; j < size; j++) {
-                            int liveCells = countLiveNeighbourCells(i, j, label);
                             Boolean isCurrentCellAlive = label[i][j].getBackground() == Color.BLACK;
-                            if (isCurrentCellAlive && (liveCells < 2)) label2[i][j].setBackground(Color.WHITE);
-                            if (isCurrentCellAlive && (liveCells > 1 && liveCells < 4)) label2[i][j].setBackground(Color.WHITE);
-                            if (isCurrentCellAlive && (liveCells > 1 && liveCells < 4)) label2[i][j].setBackground(Color.WHITE);
-
-                            else label2[i][j].setBackground(Color.BLACK);
+                            int liveNeighbourCells = countLiveNeighbourCells(i, j, label);
+                            if (isCurrentCellAlive && (liveNeighbourCells < 2 || liveNeighbourCells > 3)) label2[i][j].setBackground(Color.WHITE);
+                            if (!isCurrentCellAlive && liveNeighbourCells == 3) label2[i][j].setBackground(Color.BLACK);
                         }
                     }
 
                     remove(panel);
                     add(panel2, BorderLayout.CENTER);
-                } else {
+                } else if (panel2.isShowing()) {
                     for (int i = 0; i < size; i++) {
                         for (int j = 0; j < size; j++) {
                             int liveCells = countLiveNeighbourCells(i, j, label2);
-                            if (liveCells < 2 || liveCells > 3) label[i][j].setBackground(Color.WHITE);
-                            else label[i][j].setBackground(Color.BLACK);
+                            Boolean isCurrentCellAlive = label2[i][j].getBackground() == Color.BLACK;
+                            if (isCurrentCellAlive && (liveCells < 2 || liveCells > 3)) label[i][j].setBackground(Color.WHITE);
+                            if (!isCurrentCellAlive && liveCells == 3) label[i][j].setBackground(Color.BLACK);
                         }
                     }
 
@@ -77,19 +75,19 @@ public class Board extends JFrame {
         timer.start();
     }
 
-    public static void setPanelLabelBg(JPanel panel, JLabel[][] label) {
+    public static void setPanelLabelBg(JPanel panel, JLabel[][] label, Color color) {
         for (int i = 0; i < label.length; i++) {
             for (int j = 0; j < label.length; j++) {
                 label[i][j] = new JLabel();
                 label[i][j].setOpaque(true);
-                label[i][j].setBackground(Color.WHITE);
+                label[i][j].setBackground(color);
                 panel.add(label[i][j]);
             }
         }
     }
 
     public static void setDefaultPanelConfiguration(JPanel panel, JLabel[][] label) {
-        setPanelLabelBg(panel, label);
+        setPanelLabelBg(panel, label, Color.WHITE);
     }
 
     public static int countLiveNeighbourCells(int i, int j, JLabel[][] boards) {
