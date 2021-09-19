d05f47294f0d12f2caf0f9378f49b3f08737c785
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2021-09-19 15:16:37 +0700

    try another way to visualize the boards

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -55,9 +55,6 @@ public class Main {
             System.out.println();
 
             conwayBoards = tempBoards;
-
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
         }
     }
 
